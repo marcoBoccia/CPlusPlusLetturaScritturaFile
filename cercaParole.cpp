ce6423abb8dbc4c1c2c5d73c8c2fc59a68cd5a83
Marco Boccia <pokermark95@live.it>
2023-02-22 12:19:11 +0100

    completata logica per cercare parola e restituirla insieme alla strofa finchè non trova una linea vuota (manca restituire anche i versi precedenti)

--- cercaParole.cpp
+++ cercaParole.cpp
@@ -1,24 +1,44 @@
 // Scrivere un programma in c++ che rimane aperto finchè l'utente non digita EXIT. Il programma chiede all'utente una parola da cercare in un file. Restituiisce a schermo la strofa con i numeri di riga in cui appare la parola cercata, chiede la successiva parola a meno che non si inserisce EXIT
 
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
+int main() {
     string parola;
-    while (true)
-    {
-        cout << "Inserisci una parola da cercare (o EXIT per uscire): "<< endl;
-        cin >> parola;
-        if (parola == "EXIT"){
+    while (true) {
+        cout << "Inserisci una parola da cercare (o EXIT per uscire): " <<endl;
+        getline(cin, parola);
+        if (parola == "EXIT") {
             break;
         }
         ifstream file("../maggio.txt");
+        if (!file) {
+            cout << "Impossibile aprire il file!" << endl;
+            return 1;
+        }
         string riga;
         int numeroRiga = 1;
+        bool trovata = false;
+        while (getline(file, riga)) {
+            if (riga.find(parola) != string::npos) {
+                trovata = true;
+                cout << "La parola '" << parola << "' è presente alla riga " << numeroRiga << ": " << riga << endl;
+                int i = 1;
+                while (getline(file, riga) && !riga.empty()) {
+                    cout << numeroRiga + i<< ": " << riga << endl;
+                    i++;
+                }
+                break;
+            }
+            numeroRiga++;
+        }
+        if (!trovata) {
+            cout << "La parola '" << parola << "' non è stata trovata." << endl;
+        }
     }
-    
+    return 0;
 }
+
